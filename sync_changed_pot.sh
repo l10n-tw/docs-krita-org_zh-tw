b824f91cdb25f40fdd9d1a4723f1e2d0b1399f63
Alvin Wong <alvinhochun@gmail.com>
2021-08-06 13:40:59 +0800

    Fix path handling in script

--- sync_changed_pot.sh
+++ sync_changed_pot.sh
@@ -8,28 +8,31 @@ if [ $# -ne 2 ]; then
     exit 1
 fi
 
+dir_from=$(realpath "$1")
+dir_to=$(realpath "$2")
+
 pot_diff_unchanged() {
     diff -q -I^\"POT-Creation-Date: "$1" "$2" > /dev/null
 }
 
-pushd "$1" > /dev/null
+pushd "$dir_from" > /dev/null
 for p in **/*.pot; do
     newname=${newname//\//___}
     newname=docs_krita_org_$p
-    if [[ -f "$2/$newname" ]] && pot_diff_unchanged "$p" "$2/$newname"; then
+    if [[ -f "$dir_to/$newname" ]] && pot_diff_unchanged "$p" "$dir_to/$newname"; then
         echo "Template '$p' not changed, skipping."
         continue
     fi
-    echo "Copying '$p' to '$2/$newname'..."
-    cp "$p" "$2/$newname"
+    echo "Copying '$p' to '$dir_to/$newname'..."
+    cp "$p" "$dir_to/$newname"
 done
 popd > /dev/null
 
-pushd "$2" > /dev/null
+pushd "$dir_to" > /dev/null
 for p in **/*.pot; do
     newname=${p/docs_krita_org_/}
     newname=${newname//___/\/}
-    if [[ ! -f "$1/$newname" ]]; then
+    if [[ ! -f "$dir_from/$newname" ]]; then
         echo "Deleting POT template '$p'..."
         rm "$p"
     fi
